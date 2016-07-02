6afd54b67e2c0e697cd00c4b9c780aaf326b35c0
juliusbuckley <juliusbuckley@gmail.com>
2016-07-01 17:37:36 -0700

    Added name table to chat database.

--- server/schema.sql
+++ server/schema.sql
@@ -1,10 +1,16 @@
-CREATE DATABASE chat;
+CREATE DATABASE IF NOT EXISTS chat;
 
 USE chat;
 
+CREATE TABLE IF NOT EXISTS name (
+    id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(20)
+);
+/*
 CREATE TABLE messages (
   /* Describe your table here.*/
-);
+
+-- );
 
 /* Create other tables and define schemas for them here! */
 
